958ef79f7e4d2143507ab5749dd1941991be712e
Eric <edhavey@homemail.com>
2021-08-21 18:30:41 -0500

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, greetingType) {
+  const thankYouArr = [];
+  names.forEach((name) => {
+    thankYouArr.push(`Thank you, ${name}, for the wonderful ${greetingType} gift!`)
+  })
+  return thankYouArr;
+}
+
+function countDown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log(i);
+  }
+}
